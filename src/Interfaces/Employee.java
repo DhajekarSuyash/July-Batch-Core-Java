416752acb517aee8ea0a26aa29b6bee92b883101
DhajekarSuyash <suyash.dhajekar18@gmail.com>
2025-09-05 23:11:06 +0530

    Abstraction using interface

---
+++ src/Interfaces/Employee.java
@@ -0,0 +1,44 @@
+package Interfaces;
+
+public interface Employee {
+	
+	public void printName();
+	
+	public void printAge();
+	
+//	public void something();
+	
+	public static void xyz() {
+		System.out.println("static method");
+		
+	}
+	
+	
+	default void abc() {
+		
+	}
+	
+	private void printsalary() {
+		
+		
+	}
+
+	//1. interface dont have constructors
+//	public Employee() {
+//		
+//	}
+	
+	
+	//2. Interface cant have concrete methods
+//	public void getName() {
+//		
+//	}
+//	
+//	public int getAge() {
+//		return 4;
+//	}
+	
+	
+	
+
+}
