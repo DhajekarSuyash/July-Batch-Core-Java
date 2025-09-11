c4e1bf58fd6943721910f6f399e1d6db4ef0ed85
DhajekarSuyash <suyash.dhajekar18@gmail.com>
2025-09-11 18:36:44 +0530

    throw and throws

---
+++ src/ExceptionHandling/Demo4.java
@@ -0,0 +1,56 @@
+package ExceptionHandling;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Demo4 {
+	
+	
+	public static void main(String[] args) throws ArithmeticException
+	{
+		Demo4 d = new Demo4();
+	
+		d.div();
+		System.out.println("END");
+		
+		
+		
+	}
+	
+	public void div() throws ArithmeticException
+	{
+		int a = 12/0;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//compileTime using throw
+//	public static void main(String[] args) {
+//		Demo4 d = new Demo4();
+//		try {
+//			d.anything();
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	public void anything() throws FileNotFoundException {
+//		Demo4 d = new Demo4();
+//		d.something();
+//	}
+//
+//	public void something() throws FileNotFoundException  {
+//		FileReader reader = new FileReader("abc.txt");
+//	}
+}
